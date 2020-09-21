4d0b022d7d0306369734a367c44a7bfdf92c4a13
MilenaS17 <milena.schwinghammer@gmail.com>
2020-09-21 16:51:53 +0200

    Update 21.09.2020: Versuch Einbau DB und JasonObject für Login

--- javascript.js
+++ javascript.js
@@ -22,14 +22,18 @@ alert("TEST Registrierung");
 
 });
 
-formRegister.addEventListener("cancel", (evt) => {
+formRegister.addEventListener("button", (evt) => {
     evt.preventDefault();
     const values = Object.fromEntries(new FormData(evt.target));
 
     console.log(values);
+
+    alert("Test Cancel");
 });
 
 /* Login Formular */
+
+const formLogin = document.querySelector("form.login");
 
 formLogin.addEventListener("submit", (evt)=> {
     evt.preventDefault();
@@ -40,7 +44,7 @@ formLogin.addEventListener("submit", (evt)=> {
 
 });
 
-formLogin.addEventListener("cancel", (evt) => {
+formLogin.addEventListener("button", (evt) => {
     evt.preventDefault();
     const values = Object.fromEntries(new FormData(evt.target));
 
@@ -71,3 +75,59 @@ buttonCancel.addEventListener("click"),() =>{
     alert ("TEST");
 }
 */
+
+/* der nachfolgende Inhalt ist von: https://codeshack.io/basic-login-system-nodejs-express-mysql/ */
+const benutzer = {
+    benutzername: "Benutzer1" ,
+    passwort: "test123",
+    email: "milena.schwinghammer@mail.com"};
+
+let mysql = require("mysql");
+let express = require("express");
+let session = require("express-session");
+let bodyParser = require("body-parser");
+let path = require("path");
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodelogin'
+});
+
+let app = express();
+
+app.listen(3000);
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/', function(request, response) {
+	response.sendFile(path.join(__dirname + '/login.html'));
+});
+
+app.post('/auth', function(request, response) {
+	let username = request.body.username;
+	let password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM benutzer WHERE benutzername = ? AND passwort = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/index.html');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
